887425a73bf78f8de5797989b5c15c57f9982724
Suyash <suyashutekar11@gmail.com>
2025-08-01 20:42:14 +0530

    TABLE USING WHILE LOOP

---
+++ src/Multiplicationtable.java
@@ -0,0 +1,10 @@
+public class Multiplicationtable {
+    public static void main(String[] args){
+        int num = 7;
+        int count=1;
+        while (count<=10){
+            System.out.println(num+"X"+count+"="+num*count);
+            count++;
+        }
+    }
+}
