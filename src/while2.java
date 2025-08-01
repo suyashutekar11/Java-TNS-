6481044b63e224d5c82bbc0ab36f2b186b67b63e
Suyash <suyashutekar11@gmail.com>
2025-08-01 20:27:55 +0530

    Add number from 1 to 10 using while

---
+++ src/while2.java
@@ -0,0 +1,11 @@
+public class while2 {
+    public static void main(String[] args){
+        int i = 0;
+        int count = 0;
+        while (i<=10){
+            count = count +i;
+            i++;
+        }
+        System.out.println(count);
+    }
+}
