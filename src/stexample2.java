199e22ec0759693708bbb3f2ae5610c9a1ceaed9
Suyash <suyashutekar11@gmail.com>
2025-08-10 16:04:19 +0530

    String Palindrome

--- src/stexample2.java
+++ src/stexample2.java
@@ -1,15 +1,22 @@
+import java.util.Scanner;
+
 //palindrome
 public class stexample2 {
-    public static void main(String[] args){
-        String s1="beeb";
-        String reverse ="";
-        for (int i=s1.length()-1;i>=0;i--){
-            reverse += s1.charAt(i);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the input:");
+        String input = sc.nextLine();
+        String reverse = "";
+        for(int i =input.length()-1;i>=0;i--){
+            reverse+=input.charAt(i);
         }
-        if(reverse.equalsIgnoreCase(s1)){
-            System.out.println("it is palindrome");
+        System.out.print("reversed string: "+reverse);
+        System.out.println();
+
+        if(input.equalsIgnoreCase(reverse)){
+            System.out.println("It is a palindrome");
         }else {
-            System.out.println("not a palindrome");
+            System.out.println("It is not a palindrome");
         }
     }
 }
